e1bc710ab90c908f213b3a63edf7b862b7ceba99
Walderson Shimokawa <lord.caos@uol.com.br>
2018-02-16 01:50:11 -0300

    Evitando referência cíclica de UO
    Não que ocorra referência cíclica ao gerar a avaliação de competências.

--- include/gerarAvaliacoes.php
+++ include/gerarAvaliacoes.php
@@ -217,9 +217,12 @@ function arrayUO($conn, $id, $array) {
     $result = $stmt->get_result();
     if ($result->num_rows > 0) {
       $row = $result->fetch_assoc();
-      $array = arrayUO($conn, $row["uo_id"], $array);
-      $ind = sizeof($array);
-      $array[$ind] = $row["id"];
+      // Prossegue com a recursão, somente quando não houver referência cíclica
+      if (!in_array($row["id"], $array)) {
+        $array = arrayUO($conn, $row["uo_id"], $array);
+        $ind = sizeof($array);
+        $array[$ind] = $row["id"];
+      }
     }
   }
   return $array;
